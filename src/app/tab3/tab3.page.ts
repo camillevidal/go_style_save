325855bae761e73b067b83fe9f86a1f842e3787a
Camille_VIDAL <camillevidal58@aol.fr>
2020-03-09 16:57:45 +0100

    refresh

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -5,57 +5,61 @@ import { CouponService } from '../coupon.service';
 import { CouponByUser } from '../couponByUser';
 
 @Component({
-	selector: 'app-tab3',
-	templateUrl: 'tab3.page.html',
-	styleUrls: ['tab3.page.scss']
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
 
-	//prop qui contient les données consommées
-	coupons: Coupon
-	idCoupon: Number
-	unCoupon: Observable<Coupon>
-	couponService: CouponService
-	couponByUserList: CouponByUser[]
-	couponList: any
+  //prop qui contient les données consommées
+  coupons: Coupon
+  idCoupon: Number
+  unCoupon: Observable<Coupon>
+  couponService: CouponService
+  couponByUserList: CouponByUser[]
+  couponList: any
 
-	//coupon
-	description: String
-	code_value: String
-	end: String
-	start: String
-
-
-	constructor(couponService: CouponService) {
-		this.couponService = couponService
-		this.couponList = []
-		this.couponService.getCouponsByUser().subscribe(value => {
-			this.couponByUserList = value
-			let currentUser = localStorage.getItem("login");
-			console.log("current user"+currentUser)
-			if (currentUser != "" || currentUser != null) {
-				this.couponByUserList.forEach(coupon => {
-					if (coupon.idUser == currentUser) {
-						this.idCoupon = coupon.idCoupon
-						this.couponService.getCoupon(this.idCoupon).subscribe(result => {
-
-							this.couponList.push(result)
+  //coupon
+  description: String
+  code_value: String
+  end: String
+  start: String
 
 
+  constructor(couponService: CouponService) {
+    this.couponService = couponService
+    this.loadCouponsByUser()
+  }
 
-						})
-					}
-				})
-			}
-			
-			else {
-				//si l'utilisateur est null
-				alert("erreur lors de la récupèration de l'utilisateur")
-			}
-			console.log("len"+this.couponList.length)
-		})
-	}
+  loadCouponsByUser(refresher=null){
+    this.couponList = []
+    this.couponService.getCouponsByUser().subscribe(value => {
+      this.couponByUserList = value  
+      let currentUser = localStorage.getItem("login");
+      console.log("current user"+currentUser)
+      if (currentUser != "" || currentUser != null) {
+        this.couponByUserList.forEach(coupon => {
+          if (coupon.idUser == currentUser) {
+            this.idCoupon = coupon.idCoupon
+            this.couponService.getCoupon(this.idCoupon).subscribe(result => {
+
+              this.couponList.push(result)
+              if(refresher){
+                console.log(refresher)
+                refresher.target.complete()
+              }
+
+
+            })
+          }
+        })
+      }
+      
+      else {
+        //si l'utilisateur est null
+        alert("erreur lors de la récupèration de l'utilisateur")
+      }
+      console.log("len"+this.couponList.length)
+    })
+  }
 }
-
-
-
